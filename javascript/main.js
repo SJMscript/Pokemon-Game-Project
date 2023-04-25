986f4002923a6b999718ed377324613e9d539ef0
Santiago Massa <sjm_95@hotmail.com>
2023-04-25 14:52:36 +0200

    audio

--- javascript/main.js
+++ javascript/main.js
@@ -60,3 +60,24 @@ window.addEventListener("keydown", (event) => {
     elementosJuego.disparaDragonite()
     }
 })
+
+window.addEventListener("load",function(){
+	document.getElementById("play").addEventListener("click",sonarMusica);
+	document.getElementById("stop").addEventListener("click",pararMusica);			
+});
+
+function sonarMusica(){
+	var sonido = document.createElement("iframe");
+	sonido.setAttribute("src","mp3/pokemon-intro.mp3");
+	document.body.appendChild(sonido);
+	document.getElementById("play").removeEventListener("click",sonarMusica);
+}
+
+function pararMusica(){
+	var iframe = document.getElementsByTagName("iframe");
+
+	if (iframe.length > 0){
+		iframe[0].parentNode.removeChild(iframe[0]);
+		document.getElementById("play").addEventListener("click",sonarMusica);
+	}
+}
